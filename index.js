4883ffccfc38391958759cb87434ffd2001c2d74
Jakalgit <mistetsmit05@gmail.com>
2024-04-14 20:27:48 +0300

    commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const sendMessage = async () => {
     const text = textarea.value
     if (text.replaceAll('\n', "").length !== 0) {
         button_text.style.display = 'none'
-        loading.classList.remove('loading_none')
+        loading.classList.remove('loading-none')
         const message = {
             role: 'user',
             content: text
@@ -52,13 +52,14 @@ const sendMessage = async () => {
         const message_response = await response.json()
 
         button_text.style.display = 'block'
-        loading.classList.add('loading_none')
+        loading.classList.add('loading-none')
 
         addMessageToRibbon(message_response)
         scrollToBottom()
     }
 }
 
+const chat = document.getElementById('chat')
 const empty_text = document.getElementById('empty-text')
 const textarea = document.getElementById('textarea');
 const ribbon = document.getElementById('ribbon')
@@ -67,7 +68,11 @@ const sendButton = document.getElementById('send')
 const loading = document.getElementById('loading')
 const button_text = document.getElementById('button-text')
 const messages = []
+const open_chat_button = document.getElementById('open-chat')
+const close = document.getElementById('close')
 const URL = "http://127.0.0.1:4567"
+
+const size = document.getElementById('size')
 
 textarea.addEventListener("keydown", async function(event) {
     if (event.key === "Enter") {
@@ -83,4 +88,26 @@ textarea.addEventListener('input', (event) => {
 
 sendButton.addEventListener('click', async () => {
     sendMessage().then()
+})
+
+open_chat_button.addEventListener('click', () => {
+    chat.classList.remove('close-chat-anim')
+    chat.classList.add('open-chat-anim')
+    open_chat_button.style.opacity = '0'
+    setTimeout(() => {
+        open_chat_button.style.display = 'none'
+        chat.style.display = 'flex'
+        size.innerText = window.innerWidth.toString()
+    }, 200)
+})
+
+close.addEventListener('click', () => {
+    textarea.value = ""
+    chat.classList.remove('open-chat-anim')
+    chat.classList.add('close-chat-anim')
+    open_chat_button.style.display = 'block'
+    setTimeout(() => {
+        chat.style.display = 'none'
+        open_chat_button.style.opacity = '1'
+    }, 500)
 })
